58b2b91b460196a733f5d80516fd2d5c97949434
ffsong <cq_tans@163.com>
2019-04-28 17:05:18 +0800

    标题修改

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="float-left">
-            温故而知新，可以为师矣 <span style="color: #e27575;font-size: 14px;">❤</span>
+            吾日三省吾身 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="float-right"><a href="mailto:cq_tans@163.com">联系我们</a></p>
